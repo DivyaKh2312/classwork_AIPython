a704cc454cae7a8e674c4608305133333b7dc8f0
DivyaKh2312 <Divya.khunt@metropolia.fi>
2025-03-20 14:36:42 +0200

    compelete

---
+++ condition.py
@@ -0,0 +1,20 @@
+def medicine():
+
+
+        age = int(input("Enter the patient's age: "))
+
+        if age >= 18:
+            print("The medicine can be given.")
+        elif 15 <= age < 18:
+            weight = float(input("Enter the patient's weight (kg): "))
+            if weight >= 55:
+                print("The medicine can be given.")
+            else:
+                print("The medicine cannot be given due to insufficient weight.")
+        else:
+            print("The medicine cannot be given due to age restriction.")
+
+
+
+# Run the function
+medicine()
